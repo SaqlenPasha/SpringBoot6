e92c6fe7249f6f49144a6cc1126abb6ef578b279
SaqlenPasha <saqlenpasha47@gmail.com>
2025-04-09 21:11:45 +0530

    XMl Configuration

--- src/main/java/com/learning_spring/example/xmlConf/XmlConfigurationContextLauncherApplication.java
+++ src/main/java/com/learning_spring/example/xmlConf/XmlConfigurationContextLauncherApplication.java
@@ -1,10 +1,24 @@
 package com.learning_spring.example.xmlConf;
 
+import java.util.Arrays;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.learning_spring.example.game.GameRunner;
+
 public class XmlConfigurationContextLauncherApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		try(var context=new ClassPathXmlApplicationContext("contextConfiguration.xml")){
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			
+			System.out.println(context.getBean("name"));
+			
+			context.getBean(GameRunner.class).run();
+		}
 	}
 
 }
